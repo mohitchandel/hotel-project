5bac20a793256fabe0b0284ed73b191044edecf6
kynet <mohitchandel639@gmail.com>
2019-07-29 11:44:38 +0530

    Edit

--- booking/models.py
+++ booking/models.py
@@ -2,12 +2,17 @@ from django.db import models
 
 class Booking(models.Model):
     booking_id = models.AutoField(primary_key=True)
-    cust_name = models.CharField(max_length=50, default='')
-    arv_date = models.DateField()
-    dep_dte  = models.DateField()
-    adults   = models.CharField(max_length=20, default='')
-    child    = models.CharField(max_length=20, default='')
-    child_room = models.CharField(max_length=20, default='')
+    arval_date = models.CharField(max_length=40, default=None)
+    dep_date   = models.CharField(max_length=40, default=None)
+    persons    = models.CharField(max_length=40, default=None)
+    persons    = models.CharField(max_length=40, default=None)
+    child      = models.CharField(max_length=40, default=None)    
+    room_type  = models.CharField(max_length=40, default=None)
+    total_rooms = models.IntegerField(default=1)
+    customer_name = models.CharField(max_length=40, default=None)
+    phone_num  = models.CharField(max_length=40, default=None)
 
     def __str__(self):
-        return self.cust_name
+        return self.customer_name
+
+    
---
+++ static/js/booking.js
@@ -0,0 +1,7 @@
+//rooms values
+
+var SingleDeluxeRoom  = 200;	
+var EconomyDouble     = 200;
+var DoubleDeluxeRoom  = 250;
+var HoneymoonSuit     = 750;
+
